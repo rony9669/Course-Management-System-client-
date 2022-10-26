19c95942e5ad8e7bd86d73e422e8ae832f368eae
rony9669 <ronychowdhury.cse@gmail.com>
2022-10-26 09:12:31 +0600

    environment vaiable  added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBpQxT_C7WeNlrK8ioxCcdsTuQROTn3o00",
-  authDomain: "computer-science-departm-bc581.firebaseapp.com",
-  projectId: "computer-science-departm-bc581",
-  storageBucket: "computer-science-departm-bc581.appspot.com",
-  messagingSenderId: "75407702815",
-  appId: "1:75407702815:web:9a05af5d65b3328760ce2a",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
